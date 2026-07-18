6aeb3b23e5403947d48f9a17389a4ab2d7ce7bb9
almazi4ik <sigmakrutoi111555@gmail.com>
2026-07-18 18:46:47 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,25 +1,13 @@
 const config = {
     type: Phaser.AUTO, width: 800, height: 600,
-    scene: [Menu, Game]
+    scene: { create: create, update: update }
 };
 const game = new Phaser.Game(config);
+let socket = io();
 
-// СЦЕНА МЕНЮ
-class Menu extends Phaser.Scene {
-    constructor() { super('Menu'); }
-    create() {
-        this.add.text(400, 200, 'CraftWars.io', { fontSize: '64px', fill: '#fff' }).setOrigin(0.5);
-        let btn = this.add.text(400, 400, 'ИГРАТЬ', { fontSize: '32px', fill: '#0f0', backgroundColor: '#333', padding: {x: 20, y: 10} }).setOrigin(0.5).setInteractive();
-        
-        btn.on('pointerdown', () => this.scene.start('Game'));
-    }
+function create() {
+    this.add.text(400, 300, 'Добро пожаловать, ' + (window.playerNickname || 'Игрок'), { fill: '#fff' }).setOrigin(0.5);
+    // Здесь будет твой игровой мир
 }
 
-// СЦЕНА ИГРЫ
-class Game extends Phaser.Scene {
-    constructor() { super('Game'); }
-    create() {
-        this.add.text(10, 10, 'Вы в игре! (Нажми E для инвентаря)', { fill: '#fff' });
-        // Здесь будет ваш основной код игры
-    }
-}
+function update() {}
